f97a452dc8eb03e6b0e39f6692f547bddac55d22
kumimu <chunk1200@gmail.com>
2022-09-15 17:35:44 +0900

    未完了のリストに追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // alert(inputText);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
